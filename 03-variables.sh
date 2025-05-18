53b66041a116704cc079a0ffe8a04b587b0d9566
sreekanthp <varshisree2@gmail.com>
2025-05-18 22:46:37 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1 = Modi
+PERSON2 = Musk 
+
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I Am fine, how are you doing"
+echo "$PERSON1:: I Am fine to, What's up?"
+echo "$PERSON2:: Nothing, just going to mars now will you come ?"
+echo "$PERSON1:: Sorry, you carry on, I will come once you!"
