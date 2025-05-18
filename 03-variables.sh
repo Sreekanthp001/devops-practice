089c2a8bde16d24590b799d772c7a15f53d865b6
sreekanthp <varshisree2@gmail.com>
2025-05-18 22:48:53 +0530

    hello world started

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1 = Modi
-PERSON2 = Musk 
+PERSON1=Modi
+PERSON2=Musk 
 
 echo "$PERSON1:: Hey $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I Am fine, how are you doing"
