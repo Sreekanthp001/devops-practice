22600229a89645201e86b3a836d86c0381f70349
sreekanthp <varshisree2@gmail.com>
2025-06-14 14:56:03 +0530

    Gmail Alert

---
+++ 21-disk-usage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+Disk_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=1 # in project it will be 75
+MSG=" "
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+while IFS= read line
+do 
+
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7F}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> reprents HTML new
+    fi 
+
+done <<< $DISK_USAGE
+
+#echo -e $MSG
+
+sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "sreekanthpaleti1999@gmail.com" "ALERT-High Disk Usage"
