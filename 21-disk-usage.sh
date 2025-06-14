3a907f393d2a2973659c7086521f36c0a4da2851
sreekanthp <varshisree2@gmail.com>
2025-06-14 18:58:20 +0530

    Gmail Alert

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Disk_USAGE=$(df -hT | grep -v Filesystem)
+DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 # in project it will be 75
 MSG=" "
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
